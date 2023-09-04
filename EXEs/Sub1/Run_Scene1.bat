14b68b67d0b9d293ee1b4cae2ab924a726f89a0f
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-09-04 18:13:34 +0900

    S1/0.3 feat: 실행경로

--- EXEs/Sub1/Run_Scene1.bat
+++ EXEs/Sub1/Run_Scene1.bat
@@ -1,7 +1,6 @@
 @REM start cmd /k "D: && cd \"
 
-start "Window1" cmd.exe /k "D: && cd \Projects\P0_Webcam_AI_v2 && conda activate AItist && python main_webcam.py --index 0"
-
+start "Window1" cmd.exe /k "D: && cd \FiniteLoop\FiniteLoop_Scene1 && conda activate AItist && FiniteLoop_Scene1.exe"
 
 @REM 웹캠 8060
 @REM P3D 8072
