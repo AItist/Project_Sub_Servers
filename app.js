b48a423f143accc327f1305f16e30ddbcad386f6
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-09-04 14:56:45 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
+// 1번 컴퓨터에서 할것
+// - Scene2 프로그램 실행, 종료, 상태 확인
+// 2번 컴퓨터에서 할것
+// - Scene1 프로그램 실행, 종료, 상태 확인
+// 3번 컴퓨터에서 할것
+// - AI, P3D, Webcam 2개
+// 4번 컴퓨터(메인)에서 할것
+// - middle, merge, Webcam 2개
+
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
