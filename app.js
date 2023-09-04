c70c161edc54dfbfa4f2e3c31fc4440946447bff
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-09-04 15:02:31 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
-// 1번 컴퓨터에서 할것
+// 3001 1번 컴퓨터에서 할것
 // - Scene2 프로그램 실행, 종료, 상태 확인
-// 2번 컴퓨터에서 할것
+// 3002 2번 컴퓨터에서 할것
 // - Scene1 프로그램 실행, 종료, 상태 확인
-// 3번 컴퓨터에서 할것
+// 3003 3번 컴퓨터에서 할것
 // - AI, P3D, Webcam 2개
-// 4번 컴퓨터(메인)에서 할것
+// 3000 4번 컴퓨터(메인)에서 할것
 // - middle, merge, Webcam 2개
+
+
+function start_program(filePath) {
+
+}
+
+function stop_program(filePath) {
+    
+}
+
+function ping(filePath) {
+
+}
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
