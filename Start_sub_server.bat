7d7da254f89bf2a01ebbfc45497b7de7346733f5
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-09-04 19:15:47 +0900

    Create Start_sub_server.bat

---
+++ Start_sub_server.bat
@@ -0,0 +1,9 @@
+@REM start cmd /k "D: && cd \"
+
+start "Window1" cmd.exe /k "D: && cd \Projects\Project_Sub_Servers && npm run dev"
+
+
+@REM 웹캠 8060
+@REM P3D 8072
+@REM 메인 프로그램 8074
+@REM 메인 프로그램2 8075
