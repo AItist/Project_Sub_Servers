623816e542096a51636c9c28ab9c391824a1e6bf
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-09-04 18:19:36 +0900

    S3/0.2.1 P3D 실행파일 추가

---
+++ EXEs/Sub3/Run_P3D.bat
@@ -0,0 +1,9 @@
+@REM start cmd /k "D: && cd \"
+
+start "Window1" cmd.exe /k "D: && cd \Projects\P0_AI && conda activate AItist && python main.py --index 0"
+
+
+@REM 웹캠 8060
+@REM P3D 8072
+@REM 메인 프로그램 8074
+@REM 메인 프로그램2 8075
