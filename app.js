746b0e6ff5eaf4d67783c6a0594fed495affe6eb
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-09-04 15:40:19 +0900

    0.0.5 feat: 서브 서버에서 쓸 프로세스의 생성, 제거, 핑 코드 확인

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { exec } = require('child_process');
 const app = express();
 const PORT = 3001;
 
@@ -11,23 +12,83 @@ const PORT = 3001;
 // 3000 4번 컴퓨터(메인)에서 할것
 // - middle, merge, Webcam 2개
 
+let process;
+let isRunning = false;
 
-function start_program(filePath) {
+let debugProgramPath = 'D:\\Projects\\Project_Sub_Servers\\hello.bat';
 
+function start_process(filePath) {
+    if (!isRunning) {
+        process = exec(filePath, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+            }
+            // console.log(`stdout: ${stdout}`);
+            // console.error(`stderr: ${stderr}`);
+        });
+
+        process.on('exit', (code) => {
+            console.log(`Child exited with code ${code}`);
+            process = null;
+            isRunning = false;
+        });
+
+        isRunning = true;
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
-function stop_program(filePath) {
-    
-}
-
-function ping(filePath) {
-
+function stop_process(_process) {
+    if (_process) {
+        _process.kill();
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+    res.send('Hello, World!');
+});
+
+app.get('/start/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(`id : ${id}`);
+
+    if (start_process(debugProgramPath)) {
+        res.send(`Process ${id} started`);
+    }
+    else {
+        res.send(`Process ${id} already running`);
+    }
+});
+
+app.get('/stop/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(`id : ${id}`);
+
+    if (stop_process(process)) {
+        res.send(`Process ${id} killed`);
+        process = null;
+        isRunning = false;
+    }
+    else {
+        res.send(`Process ${id} not found`);
+    }
+});
+
+app.get('/status/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(`id : ${id}`);
+
+    res.send(isRunning ? `Process ${id} is running` : `Process ${id} is not running`);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
---
+++ hello.bat
@@ -0,0 +1,1 @@
+start "Window1" cmd.exe /k "echo hello"
