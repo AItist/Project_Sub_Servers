f7b3e32dc807e87d83af6542e3168b6ba154efa5
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-09-05 17:27:52 +0900

    Sub2

---
+++ EXEs/Sub2/Run_Scene2_Vertical.bat
@@ -0,0 +1,8 @@
+@REM start cmd /k "D: && cd \"
+
+start "Window1" cmd.exe /k "D: && cd \FiniteLoop\FiniteLoop_Scene2_Vertical && FiniteLoop_Scene2.exe"
+
+@REM 웹캠 8060
+@REM P3D 8072
+@REM 메인 프로그램 8074
+@REM 메인 프로그램2 8075
