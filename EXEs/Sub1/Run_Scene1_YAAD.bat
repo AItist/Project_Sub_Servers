22e387219a33d47da745f66ce21cb0483775c4d9
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-09-05 17:23:02 +0900

    Sub1

---
+++ EXEs/Sub1/Run_Scene1_YAAD.bat
@@ -0,0 +1,8 @@
+@REM start cmd /k "D: && cd \"
+
+start "Window1" cmd.exe /k "D: && cd \FiniteLoop\FiniteLoop_Scene1_YAAD && FiniteLoop_Scene1.exe"
+
+@REM 웹캠 8060
+@REM P3D 8072
+@REM 메인 프로그램 8074
+@REM 메인 프로그램2 8075
