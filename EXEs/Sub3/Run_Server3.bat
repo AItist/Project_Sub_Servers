f7cc133bfa3061f0e2dff166650f8400cfd1dc11
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-09-06 23:55:57 +0900

    sub3 연결 터미널 제거 코드 추가

--- EXEs/Sub3/Run_Server3.bat
+++ EXEs/Sub3/Run_Server3.bat
@@ -1,5 +1,5 @@
 
-start /min "Window1" cmd.exe /k "D: && cd \Projects\Project_Sub_Servers\EXEs\Sub3 && RunCam1.bat"
+start /min "Window1" cmd.exe /k "D: && cd \Projects\Project_Sub_Servers\EXEs\Sub3 && RunCam1.bat && exit"
 echo "RunCam1 started"
-start /min "Window1" cmd.exe /k "D: && cd \Projects\Project_Sub_Servers\EXEs\Sub3 && Run_AI.bat"
+start /min "Window1" cmd.exe /k "D: && cd \Projects\Project_Sub_Servers\EXEs\Sub3 && Run_AI.bat && exit"
 echo "Run_AI started"
