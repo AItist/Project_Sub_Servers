c478ecc41ae221abbcbaeba81a3baedba6f05217
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-09-06 09:56:28 +0900

    Create Run_Server3.bat

---
+++ EXEs/Sub3/Run_Server3.bat
@@ -0,0 +1,5 @@
+
+start /min "Window1" cmd.exe /k "D: && cd \Projects\Project_Sub_Servers\EXEs\Sub3 && RunCam1.bat"
+echo "RunCam1 started"
+start /min "Window1" cmd.exe /k "D: && cd \Projects\Project_Sub_Servers\EXEs\Sub3 && Run_AI.bat"
+echo "Run_AI started"
