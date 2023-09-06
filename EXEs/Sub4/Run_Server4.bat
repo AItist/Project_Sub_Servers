159a79b95054d8d48dcafbe5adc85fd854f7454a
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-09-06 09:54:28 +0900

    Create Run_Server4.bat

---
+++ EXEs/Sub4/Run_Server4.bat
@@ -0,0 +1,7 @@
+
+start /min "Window1" cmd.exe /k "D: && cd \Projects\Project_Sub_Servers\EXEs\Sub4 && RunCam3.bat"
+echo "RunCam3 started"
+start /min "Window1" cmd.exe /k "D: && cd \Projects\Project_Sub_Servers\EXEs\Sub4 && p42_P2_middle.bat"
+echo "p42_P2_middle started"
+start /min "Window1" cmd.exe /k "D: && cd \Projects\Project_Sub_Servers\EXEs\Sub4 && p43_P4_merge.bat"
+echo "p43_P4_merge started"
