cfe5f9ffa5310dfcdd0d22623ae7d50fb312668b
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-09-06 09:59:44 +0900

    Create Run_Server2.bat

---
+++ EXEs/Sub2/Run_Server2.bat
@@ -0,0 +1,5 @@
+
+start /min "Window1" cmd.exe /k "D: && cd \Projects\Project_Sub_Servers\EXEs\Sub2 && RunCam3.bat"
+echo "RunCam3 started"
+start /min "Window1" cmd.exe /k "D: && cd \Projects\Project_Sub_Servers\EXEs\Sub2 && Run_Scene1_Vertical.bat"
+echo "Run_Scene1_Vertical started"
