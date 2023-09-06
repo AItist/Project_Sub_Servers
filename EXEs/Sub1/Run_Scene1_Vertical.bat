22728486a8c05f18c14b78a9bd1b828c921adb48
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-09-06 09:38:36 +0900

    sub1 최소화 적용

--- EXEs/Sub1/Run_Scene1_Vertical.bat
+++ EXEs/Sub1/Run_Scene1_Vertical.bat
@@ -1,6 +1,6 @@
 @REM start cmd /k "D: && cd \"
 
-start "Window1" cmd.exe /k "D: && cd \FiniteLoop\FiniteLoop_Scene1_Vertical && FiniteLoop_Scene1.exe"
+start /min "Window1" cmd.exe /k "D: && cd \FiniteLoop\FiniteLoop_Scene1_Vertical && FiniteLoop_Scene1.exe"
 
 @REM 웹캠 8060
 @REM P3D 8072
