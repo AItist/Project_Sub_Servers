1f2aa82f9be84bf3becdd6799bbe445d5b022476
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-09-09 09:26:04 +0900

    Update Kill_Scenes.bat

--- EXEs/Sub2/Kill_Scenes.bat
+++ EXEs/Sub2/Kill_Scenes.bat
@@ -1,7 +1,7 @@
 @REM start cmd /k "D: && cd \"
 
 start /min "Window1" cmd.exe /k "taskkill /F /IM FiniteLoop_Scene1.exe && exit"
-start /min "Window1" cmd.exe /k "taskkill /F /IM FiniteLoop_Scene2.exe && exit"
+@REM start /min "Window1" cmd.exe /k "taskkill /F /IM FiniteLoop_Scene2.exe && exit"
 
 @REM 웹캠 8060
 @REM P3D 8072
